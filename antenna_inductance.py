29416ffcaf9e05eed515b2f14fc6bc8b88aee1f4
ext:(Miranda%20Cross) <mirandakcross@gmail.com>
2015-10-08 19:01:36 -0700

    10/8: added line antenna equation

--- antenna_inductance.py
+++ antenna_inductance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-Paying off Credit Card Debt in a Year
+Antenna Inductance
 Params: 
 Return: ?
 Definition:  
@@ -9,4 +9,35 @@ Definition:
 
 import math
 
-math.sqrt(
+def LineAntenna(radius):
+	magnetic_inductance = (.000001257 * 10) / (2 * math.pi * radius)
+	return magnetic_inductance
+
+
+def CoilAntenna (radius):
+	magnetic_inductance = (.000001257 * 10 * (radius)**2 ) / (2 * math.sqrt((radius ** 2 + ))
+
+
+
+def main():
+	antenna_type = raw_input('Antenna Type: ')
+	minimum_distance = float(raw_input('What is the minimum distance value?: '))
+	maximum_distance = float(raw_input('What is the maximum distance value?: '))
+	
+	radius = maximum_distance - minimum_distance
+	r = radius
+	x = radius
+	
+	if antenna_type == 'Line Antenna':
+		print LineAntenna (r)
+	
+	
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+

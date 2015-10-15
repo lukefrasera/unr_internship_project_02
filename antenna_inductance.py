f71d2cef754e4e9003a408f456023d60778e06cb
"Luke Fraser" <lukefrasera@gmail.com>
2015-10-15 14:39:11 -0700

    making it run without syntax errors

--- antenna_inductance.py
+++ antenna_inductance.py
@@ -15,7 +15,7 @@ def LineAntenna(r, I):
 
 
 def CoilAntenna(I, R, x):
-	return (1.257e-6 * I * (R)**2 ) / (2 * math.sqrt((R**2 + x**2)**3)
+	return (1.257e-6 * I * (R)**2 ) / (2 * math.sqrt((R**2 + x**2)**3))
 
 def RectangularAntenna(N, I, a, b):
 	return ((1.257e-6 * N * I * a * b) / (4 * math.pi * math.sqrt((a/2)**2 + (b/2)**2 + x**2))) * ((((a/2)**2 + x**2)**-1) + ((b/2)**2 + x**2)**-1)
@@ -29,10 +29,10 @@ def RectangularAndLineAntenna (a, b, x):
 
 def main():
 
-	antenna_type = raw_input('Antenna Type: ')
-	minimum_distance = float(raw_input('What is the minimum distance value?: '))
-	maximum_distance = float(raw_input('What is the maximum distance value?: '))
-	
+    antenna_type = raw_input('Antenna Type: ')
+    minimum_distance = float(raw_input('What is the minimum distance value?: '))
+    maximum_distance = float(raw_input('What is the maximum distance value?: '))
+
 
     #linear
     I = 10
@@ -50,34 +50,34 @@ def main():
     #second coil            
     N2 = 200
     R2 = .15
-	
-        '''
-        I = current
-        N = # of windings
-        R = radius of coil antenna
-        a = width of rectangular antenna
-        b = length of rectangular antenna
-        x = distance between 2 antennae
-        '''
+
+    '''
+    I = current
+    N = # of windings
+    R = radius of coil antenna
+    a = width of rectangular antenna
+    b = length of rectangular antenna
+    x = distance between 2 antennae
+    '''
 
     if antenna_type == 'Line Antenna':
-		r = maximum_distance
-		while (r > minimum_distance):
-			print LineAntenna(r, I)
-			r -= .05
-			
-	if antenna_type == 'Coil Antenna'
-		x = maximum_distance
-		while (x > minimum_distance):
-			print CoilAntenna(I, R1, x)
-			print TwoCoilAntennae(N1, R1, N2, R2, x)
-			x -= .05	
+        r = maximum_distance
+        while (r > minimum_distance):
+            print LineAntenna(r, I)
+            r -= .05
+
+    if antenna_type == 'Coil Antenna':
+        x = maximum_distance
+        while (x > minimum_distance):
+            print CoilAntenna(I, R1, x)
+            print TwoCoilAntennae(N1, R1, N2, R2, x)
+            x -= .05	
 
 
 
 
 
 
-	if __name__ == '__main__':
+if __name__ == '__main__':
 	main()
 

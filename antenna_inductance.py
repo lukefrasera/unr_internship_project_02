c8dfd560439a192e73daa9d5c59645cd0ed34c10
ext:(Miranda%20Cross) <mirandakcross@gmail.com>
2015-10-18 21:49:25 -0700

    10/18: Program runs without error, finished

--- antenna_inductance.py
+++ antenna_inductance.py
@@ -9,15 +9,14 @@ Definition:
 
 import math
 
-def LineAntenna(r, I):
-	magnetic_inductance = (1.257e-6 * I) / (2 * math.pi * r)
-	return magnetic_inductance
+def LineAntennaInductance(r, I):
+	return (1.257e-6 * I) / (2 * math.pi * r)
 
 
-def CoilAntenna(I, R, x):
+def CoilAntennaInductance(I, R, x):
 	return (1.257e-6 * I * (R)**2 ) / (2 * math.sqrt((R**2 + x**2)**3))
 
-def RectangularAntenna(N, I, a, b):
+def RectangularAntennaInductance(N, I, a, b, x):
 	return ((1.257e-6 * N * I * a * b) / (4 * math.pi * math.sqrt((a/2)**2 + (b/2)**2 + x**2))) * ((((a/2)**2 + x**2)**-1) + ((b/2)**2 + x**2)**-1)
 
 def TwoCoilAntennae (N1, R1, N2, R2, x):
@@ -25,7 +24,7 @@ def TwoCoilAntennae (N1, R1, N2, R2, x):
 
 
 def RectangularAndLineAntenna (a, b, x):
-	return ((1.257e-6 * a) / (2 * math.pi)) * math.loglp((x + b) / x)
+	return ((1.257e-6 * a) / (2 * math.pi)) * math.log1p((x + b) / x)
 
 def main():
 
@@ -62,17 +61,27 @@ def main():
 
     if antenna_type == 'Line Antenna':
         r = maximum_distance
-        while (r > minimum_distance):
-            print LineAntenna(r, I)
+        while (r >= minimum_distance):
+            print 'Distance:' , r
+            print 'Line Antenna Inductance:' , LineAntenna(r, I)
             r -= .05
 
     if antenna_type == 'Coil Antenna':
         x = maximum_distance
-        while (x > minimum_distance):
-            print CoilAntenna(I, R1, x)
-            print TwoCoilAntennae(N1, R1, N2, R2, x)
+        while (x >= minimum_distance):
+            print 'Distance:' , x 
+            print 'Coil Antenna Inductance:' , CoilAntenna(I, R1, x)
+            print 'Mutual Inductance' , TwoCoilAntennae(N1, R1, N2, R2, x)
+            x -= .05
+            
+            
+    if antenna_type == 'Rectangular Antenna':
+        x = maximum_distance
+        while (x >= minimum_distance):
+            print 'Distance:' , x 
+            print 'Rectangular Antenna Inductance:' , RectangularAntennaInductance(N, I, a, b, x)
+            print 'Mutual Inductance:' , RectangularAndLineAntenna(a, b, x)
             x -= .05	
-
 
 
 

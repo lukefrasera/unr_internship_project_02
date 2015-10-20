2b2ea713a05897a1f7d06f13e41e14fba03f030f
ext:(Miranda%20Cross) <mirandakcross@gmail.com>
2015-10-20 14:32:37 -0700

    10/20: Finished without indentation errors, used spaces

--- paying_off_debt.py
+++ paying_off_debt.py
@@ -8,43 +8,35 @@ Definition: Calculates the monthly payment (multiple of ten) needed to pay off a
 '''
 
 
-	
 def MonthlyPayment(outstanding_balance, monthly_interest_rate):
-	
-	monthly_payment = 10.0
-	remaining_balance = outstanding_balance
-	debt = True
-	
-	while debt:
-		
-		monthly_payment += 10
-
+    monthly_payment = 0.0
+    number_of_months = 0
+    debt = True
+    while debt:
+        monthly_payment += 10
+        remaining_balance = outstanding_balance
         for i in xrange(12):
-            remaining_balance *= (1.0 + monthly_interest_rate)
-            remaining_balance -= monthly_payment
             if remaining_balance > 0:
                 remaining_balance *= (1.0 + monthly_interest_rate)
                 remaining_balance -= monthly_payment
-
             else:
-				number_of_months = i
-				debt = False
-				break
-		
-		
-	return monthly_payment, remaining_balance, number_of_months
-	
-	
+                number_of_months = i
+                debt = False
+                break
+
+
+    return monthly_payment, remaining_balance, number_of_months
+
 
 def main ():
 
-	outstanding_balance = float(raw_input('Enter the outstanding balance on your credit card: '))
-	annual_interest_rate = float(raw_input('Enter the annual credit card interest rate as a decimal: '))
-	monthly_interest_rate = annual_interest_rate / 12
-	
-	monthly_payment, remaining_balance, number_of_months = FindMinimumPaymentOneYear(balance, monthly_interest_rate)
+    outstanding_balance = float(raw_input('Enter the outstanding balance on your credit card: '))
+    annual_interest_rate = float(raw_input('Enter the annual credit card interest rate as a decimal: '))
+    monthly_interest_rate = annual_interest_rate / 12
 
-	print "RESULT:"
+    monthly_payment, remaining_balance, number_of_months = MonthlyPayment(outstanding_balance, monthly_interest_rate)
+
+    print "RESULT:"
     print "Monthly payment to pay off debt in 1 year: ", monthly_payment
     print "Balance: ", "{0:0.2f}".format(remaining_balance)
     print "Number of months needed: ", number_of_months
